509f2dcc26e75b8de000ead8ab392b9e9e90c2c3
celialevelab <146108835+celialevelab@users.noreply.github.com>
2024-01-31 22:20:49 +0100

    music player demo version

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+
+  let greeting = "";
+  
+  if (currentHour < 12 && currentHour >= 6 ) {
+    greeting = "¡Buenos días!";
+  } else if (currentHour < 19 && currentHour >= 12) {
+    greeting = " ¡Buenas tardes!";
+  } else {
+    greeting = "¡Buenas noches!";
+  }
+</script>
+
+<h2 class="text-3xl font-bold">{greeting}</h2>
